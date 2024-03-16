b68130f1ac9fb88067c851d3ab1cebb2a5a7fe08
hslaksan <154474734+hasithasandunlakshan@users.noreply.github.com>
2024-03-16 19:00:40 +0530

    Add files via upload
    first stage done

---
+++ main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        GameName game = new GameName();
+        game.gameStart();
+        Interface test = new Interface();
+
+        // register user as a player
+        test.registerPlayer();
+        Player player = test.player;
+        test.createArmy(player);
+        test.doYouWantBuyEquipment(player);
+
+        test.printArmy(player);
+        Player challenger = test.selectPlayer();
+        boolean challengeAccepted = test.defaultPlayer(challenger);
+        String ground = test.selectHomeLand();
+
+        Battle battleField = new Battle();
+        if (challengeAccepted) {
+            battleField.Challenge(challenger, player, ground);
+            test.sellSoldiers(player);
+        } else {
+            game.animatePrint("You Left the Battle", GameName.ANSI_RED);
+        }
+
+    }
+}
